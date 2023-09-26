9435888bccef5a95b0c1892a059fa256ba402fe5
Olof Olsson <olof.olsson@snd.gu.se>
2023-09-26 13:52:34 +0200

    add logical product

--- js/app.js
+++ js/app.js
@@ -7,25 +7,38 @@ class Col{
     hasIndendedDataType
     role
     constructor(id){
-        this.id = '#'+id
-        this.displayLabel = id
+        this.id = id
+        if(isNaN(id)){
+            this.displayLabel = id
+        }
+        
+    }
+    toJSON(){
+        return {
+            '@id': '#'+this.id,
+            '@type': 'InstanceVariable',
+            displayLabel: this.displayLabel,
+            descriptiveText: this.descriptiveText,
+            hasIndendedDataType: this.hasIndendedDataType
+        }
     }
 }
 
 createApp({
     mounted(){
-        var head = CSVToArray(this.rawData, ',')[0]
-        head.forEach((id) =>{ console.log(id); this.columns.push(new Col(id))});
+        var csv = CSVToArray(this.rawData, this.delimiter)
+        csv[0].forEach((id) =>{this.columns.push(new Col(id))});
     },
     setup() {
+        const delimiter = ','
         const lang = reactive({id:'en', label: 'English'})
         const rawData = reactive("Frequency,Year,Age Cohort,Sex,Status,Median Income (USD)\nA,2003,C,M,ACT,5500\nA,2003,G,F,ACT,7500\nA,2004,E,M,EST,10000\nA,2005,B,F,ACT,14000\nA,2004,B,M,EST,2000")
         const parsedData = computed(() => {
-            return CSVToArray(rawData, ',')
+            return CSVToArray(rawData, delimiter)
         })
         const cv = {
             colRoles : [{id:'Dimension'}, {id:'Attribute'}, {id:'Measure'}],
-            colTypes : [{id:'Coded'}, {id:'Numeric'}, {id:'Time'}, {id:'Text'}]
+            colTypes : [{id:'Coded', uri:"https://www.w3.org/2009/08/skos-reference/skos.html#ConceptScheme"}, {id:'Numeric'}, {id:'Time'}, {id:'Text'}]
         }
         const columns = reactive([])
 
@@ -34,8 +47,19 @@ createApp({
                 '@context': "http://ddialliance.org/Specification/DDI-CDI/1.0/RDF/",
                 '@graph':[]
             }
-            cdi['@graph'] = columns
+            var logicalRecord = {
+                '@id' : "#logicalRecord",
+                "@type": "LogicalRecord",
+                'has' : []
+            }
 
+            for(const c of columns){
+                logicalRecord['has'].push({'@id': c.id})
+            }
+
+            cdi['@graph'].push(columns)
+            cdi['@graph'].push(logicalRecord)
+        
             return JSON.stringify(cdi, null, 2)
         })
 
