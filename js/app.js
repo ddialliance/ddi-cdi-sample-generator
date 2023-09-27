7a44e236b0a52aa5f2318007a40cd379460869ae
Olof Olsson <olof.olsson@snd.gu.se>
2023-09-27 18:07:12 +0200

    save button

--- js/app.js
+++ js/app.js
@@ -60,6 +60,27 @@ createApp({
             };
             input.click();
         },
+        saveCdi(){
+            var textFileAsBlob = new Blob([ this.cdiOutput ], { type: 'text/plain' });
+            //TODO: generate smart filename
+            var fileNameToSaveAs = "cdi.txt"; //filename.extension
+          
+            var downloadLink = document.createElement("a");
+            downloadLink.download = fileNameToSaveAs;
+            downloadLink.innerHTML = "Download File";
+            if (window.webkitURL != null) {
+              // Chrome allows the link to be clicked without actually adding it to the DOM.
+              downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+            } else {
+              // Firefox requires the link to be added to the DOM before it can be clicked.
+              downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+              downloadLink.onclick = destroyClickedElement;
+              downloadLink.style.display = "none";
+              document.body.appendChild(downloadLink);
+            }
+          
+            downloadLink.click();
+        },
         loadExample(example){
             this.input.raw = example.raw
             this.input.id = example.id
