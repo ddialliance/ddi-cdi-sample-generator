45b8751294d7d8bc5ada95f175be4c4cccf6392e
Olof Olsson <olof.olsson@snd.gu.se>
2023-10-10 17:04:54 +0200

    some minor code cleaning

--- js/app.js
+++ js/app.js
@@ -3,9 +3,9 @@ const { createApp, ref, reactive, computed, Utils } = Vue
 createApp({
     methods:{
         openCsv(){
-            let inputElement = document.createElement('input')
-            inputElement.type = 'file'
-            inputElement.accept = '.csv,.tsv'
+            let inputElement = document.createElement("input")
+            inputElement.type = "file"
+            inputElement.accept = ".csv,.tsv"
             inputElement.onchange = _ => {
                 let file = Array.from(inputElement.files)[0];
                 this.input.fileName = file.name
@@ -13,7 +13,7 @@ createApp({
                 this.input.type = file.type
                 this.input.size = file.size
                 var reader = new FileReader()
-                reader.readAsText(file, 'UTF-8')
+                reader.readAsText(file, "UTF-8")
 
                 reader.onload = readerEvent => {
                     var content = readerEvent.target.result
@@ -48,7 +48,7 @@ createApp({
         },
         reloadCsv(){
             if(this.input.fileName == null) return;
-            this.input.fileNameWithoutExtension = this.input.fileName.split('.').slice(0, -1).join('.');
+            this.input.fileNameWithoutExtension = this.input.fileName.split(".").slice(0, -1).join(".");
             console.log(this.input.fileNameWithoutExtension)
             var csv = CSVToArray(this.input.raw, this.input.delimiter)
             
@@ -68,45 +68,21 @@ createApp({
         this.reloadCsv()
     },
     setup() {
-        const lang = reactive({id:'en', label: 'English'})
-        const codeListVariableIndex = ref(null);
-        const examples = [
-            {
-                fileName: 'test.csv',
-                raw:"Frequency,Year,Age Cohort,Sex,Status,Median Income (USD)\nA,2003,C,M,ACT,5500\nA,2003,G,F,ACT,7500\nA,2004,E,M,EST,10000\nA,2005,B,F,ACT,14000\nA,2004,B,M,EST,2000"
-            },
-            {
-                fileName: 'tiny.csv',
-                raw:"id,name,value,some date\n0,Pelle,13,2010-01-12\n1,Claus,15,2020-10-10"
-            },
-            {
-                fileName: 'spss_example.csv',
-                raw: "RID,MARST,PWT\n10000001,3,537\m10000002,1,231\n10000003,2,599\n10000004,1,4003\n10000005,2,598"
-            },
-            {
-                fileName: 'canada_juvenile_crime.csv',
-                raw: "Offense,Year,Geography,TotalNumber of Cases\nTotal guilty cases - sentences,2017/2018,Canada,14227\nTotal guilty cases - sentences, 2018/2019,Canada,12167\nTotal guilty cases - sentences,2019/202,Canada,10861\nTotal guilty cases - sentences,2020/2021,Canada,6594\nTotal guilty cases - sentences,2021/2022,Canada,4688\nIntensive rehabilitation custody and supervision,2017/2018,Canada,7\nIntensive rehabilitation custody and supervision,2018/2019,Canada,5\nIntensive rehabilitation custody and supervision,2019/2020,Canada,5\nIntensive rehabilitation custody and supervision,2020/2021,Canada,12\nIntensive rehabilitation custody and supervision,2021/2022,Canada,7\nCustody,2017/2018,Canada,1811\nCustody,2018/2019,Canada,1457\nCustody,2019/2020,Canada,1260\nCustody,2020/2021,Canada,653\nCustody,2021/2022,Canada,402\nConditional sentence,2017/12018,Canada,10\nConditional sentence,2018/12019,Canada,8\nConditional sentence,2019/12020,Canada,15\nConditional sentence,2020/12021,Canada,4\nConditional sentence,2021/12022,Canada,12\nDeferred custody and supervision,2017/2018,Canada,670\nDeferred custody and supervision,2018/2019,Canada,527\nDeferred custody and supervision,2019/2020,Canada,527\nDeferred custody and supervision,2020/2021,Canada,297\nDeferred custody and supervision,2021/2022,Canada,228\nIntensive support and supervision,2017/2018,Canada,117\nIntensive support and supervision,2018/2019,Canada,124\nIntensive support and supervision,2019/2020,Canada,99\nIntensive support and supervision,2020/2021,Canada,63\nIntensive support and supervision,2021/2022,Canada,66\nAttend a non-residential program,2017/2018,Canada,98\nAttend a non-residential program,2018/2019,Canada,63\nAttend a non-residential program,2019/2020,Canada,60\nAttend a non-residential program,2020/2021,Canada,28\nAttend a non-residential program,2021/2022,Canada,11\nProbation,2017/2018,Canada,7154\nProbation,2018/2019,Canada,6195\nProbation,2019/2020,Canada,5572\nProbation,2020/2021,Canada,3411\nProbation,2021/2022,Canada,2449\nFine,2017/2018,Canada,285\nFine,2018/2019,Canada,224\nFine,2019/2020,Canada,179\nFine,2020/2021,Canada,133\nFine,2021/2022,Canada,102"
-            }
-        ]
+        const codeListVariableIndex = ref(null)
+        const examples = getCsvExamples()
         const input = reactive({
             fileName: null,
+            raw: "",
             fileNameWithoutExtension: "",
-            type: 'text/csv',
-            delimiter: ',',
+            type: "text/csv",
+            delimiter: ",",
             firstRowIsHeader: true,
             columns: [],
-            recordCount : 0,
-            raw:""
+            recordCount : 0
         })
         const cv = {
-            colRoles : [{id:'Dimension'}, {id:'Attribute'}, {id:'Measure'}],
-            colTypes : [
-                {label:'String', id: "http://rdf-vocabulary.ddialliance.org/cv/DataType/1.1.2/#String"},
-                {label:'Integer', id: "http://rdf-vocabulary.ddialliance.org/cv/DataType/1.1.2/#Integer"}, 
-                {label:'Double', id: "http://rdf-vocabulary.ddialliance.org/cv/DataType/1.1.2/#Double"}, 
-                {label:'Date', id: "http://rdf-vocabulary.ddialliance.org/cv/DataType/1.1.2/#Date"},
-                {label:'DateTime', id: "http://rdf-vocabulary.ddialliance.org/cv/DataType/1.1.2/#DateTime"}
-            ]
+            colRoles : [{id: "Dimension"}, {id: "Attribute"}, {id: "Measure"}],
+            colTypes : getColTypes()
         }
 
         const haveCodeLists = computed(() =>{
@@ -115,17 +91,17 @@ createApp({
 
         const output = computed(() => {
             return {
-                'cdi' : toDdiCdiJsonLd(input),
-                'ddiC' : toDdiCXml(input),
-                'ddiL' : toDdiLXml(input)
+                cdi : toDdiCdiJsonLd(input),
+                ddiC : toDdiCXml(input),
+                ddiL : toDdiLXml(input)
             }
         })
 
         const outputFormated = computed(() => {
             return {
-                'cdi' : (hljs.highlight(toDdiCdiJsonLd(input), { language: 'json' }).value),
-                'ddiC' : (hljs.highlight(toDdiCXml(input), { language: 'xml' }).value),
-                'ddiL' : (hljs.highlight(toDdiLXml(input), { language: 'xml' }).value),
+                cdi : (hljs.highlight(toDdiCdiJsonLd(input), { language: "json" }).value),
+                ddiC : (hljs.highlight(toDdiCXml(input), { language: "xml" }).value),
+                ddiL : (hljs.highlight(toDdiLXml(input), { language: "xml" }).value),
             }
         })
 
@@ -133,4 +109,4 @@ createApp({
             input, output, outputFormated, codeListVariableIndex, haveCodeLists, cv, examples
         }
     }
-}).mount('#app')
+}).mount("#app")
--- js/lib/utils.js
+++ js/lib/utils.js
@@ -224,6 +224,37 @@ function getColumnValues(csv, columnIndex, haveHeader){
     return column;
 }
 
+function getCsvExamples(){
+    return [
+        {
+            fileName: "test.csv",
+            raw:"Frequency,Year,Age Cohort,Sex,Status,Median Income (USD)\nA,2003,C,M,ACT,5500\nA,2003,G,F,ACT,7500\nA,2004,E,M,EST,10000\nA,2005,B,F,ACT,14000\nA,2004,B,M,EST,2000"
+        },
+        {
+            fileName: "tiny.csv",
+            raw:"id,name,value,some date\n0,Pelle,13,2010-01-12\n1,Claus,15,2020-10-10"
+        },
+        {
+            fileName: "spss_example.csv",
+            raw: "RID,MARST,PWT\n10000001,3,537\m10000002,1,231\n10000003,2,599\n10000004,1,4003\n10000005,2,598"
+        },
+        {
+            fileName: "canada_juvenile_crime.csv",
+            raw: "Offense,Year,Geography,TotalNumber of Cases\nTotal guilty cases - sentences,2017/2018,Canada,14227\nTotal guilty cases - sentences, 2018/2019,Canada,12167\nTotal guilty cases - sentences,2019/202,Canada,10861\nTotal guilty cases - sentences,2020/2021,Canada,6594\nTotal guilty cases - sentences,2021/2022,Canada,4688\nIntensive rehabilitation custody and supervision,2017/2018,Canada,7\nIntensive rehabilitation custody and supervision,2018/2019,Canada,5\nIntensive rehabilitation custody and supervision,2019/2020,Canada,5\nIntensive rehabilitation custody and supervision,2020/2021,Canada,12\nIntensive rehabilitation custody and supervision,2021/2022,Canada,7\nCustody,2017/2018,Canada,1811\nCustody,2018/2019,Canada,1457\nCustody,2019/2020,Canada,1260\nCustody,2020/2021,Canada,653\nCustody,2021/2022,Canada,402\nConditional sentence,2017/12018,Canada,10\nConditional sentence,2018/12019,Canada,8\nConditional sentence,2019/12020,Canada,15\nConditional sentence,2020/12021,Canada,4\nConditional sentence,2021/12022,Canada,12\nDeferred custody and supervision,2017/2018,Canada,670\nDeferred custody and supervision,2018/2019,Canada,527\nDeferred custody and supervision,2019/2020,Canada,527\nDeferred custody and supervision,2020/2021,Canada,297\nDeferred custody and supervision,2021/2022,Canada,228\nIntensive support and supervision,2017/2018,Canada,117\nIntensive support and supervision,2018/2019,Canada,124\nIntensive support and supervision,2019/2020,Canada,99\nIntensive support and supervision,2020/2021,Canada,63\nIntensive support and supervision,2021/2022,Canada,66\nAttend a non-residential program,2017/2018,Canada,98\nAttend a non-residential program,2018/2019,Canada,63\nAttend a non-residential program,2019/2020,Canada,60\nAttend a non-residential program,2020/2021,Canada,28\nAttend a non-residential program,2021/2022,Canada,11\nProbation,2017/2018,Canada,7154\nProbation,2018/2019,Canada,6195\nProbation,2019/2020,Canada,5572\nProbation,2020/2021,Canada,3411\nProbation,2021/2022,Canada,2449\nFine,2017/2018,Canada,285\nFine,2018/2019,Canada,224\nFine,2019/2020,Canada,179\nFine,2020/2021,Canada,133\nFine,2021/2022,Canada,102"
+        }
+    ]
+}
+
+function getColTypes(){
+    return [
+        {label: "Text", id: "http://rdf-vocabulary.ddialliance.org/cv/DataType/1.1.2/#String"},
+        {label: "Integer", id: "http://rdf-vocabulary.ddialliance.org/cv/DataType/1.1.2/#Integer"}, 
+        {label: "Double", id: "http://rdf-vocabulary.ddialliance.org/cv/DataType/1.1.2/#Double"}, 
+        {label: "Date", id: "http://rdf-vocabulary.ddialliance.org/cv/DataType/1.1.2/#Date"},
+        {label: "DateTime", id: "http://rdf-vocabulary.ddialliance.org/cv/DataType/1.1.2/#DateTime"}
+    ]
+}
+
 function guessType(values){
     const base = "http://rdf-vocabulary.ddialliance.org/cv/DataType/1.1.2/#";     
     
