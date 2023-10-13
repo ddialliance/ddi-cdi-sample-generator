f204fb87443382ec206dff2938f3f0c0bd28b227
Oliver Hopt <oliver.hopt@gesis.org>
2023-10-13 12:20:06 +0200

    first few things on lifecycle

--- js/lib/formatters/ddi-l-xml.js
+++ js/lib/formatters/ddi-l-xml.js
@@ -1,6 +1,32 @@
 function toDdiLXml(input){
-    var ddil = '<codeBook xmlns="ddi:codebook:2_5" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xs="http://www.w3.org/2001/XMLSchema" xsi:schemaLocation="ddi:codebook:2_5 http://www.ddialliance.org/Specification/DDI-Codebook/2.5/XMLSchema/codebook.xsd">'
+    var nsddi = "ddi:instance:3_3"
+    var nsr = "ddi:reusable:3_3"
+    var nspi = "ddi:physicalinstance:3_3"
+
+    var xmlDoc = document.implementation.createDocument(nsddi, "ddi:FragmentInstance", null);
+
+    var fragmentInstance = xmlDoc.getElementsByTagName("ddi:FragmentInstance")[0]
+    
+    fragmentInstance.setAttribute("xmlns:r", "ddi:reusable:3_3")
+    fragmentInstance.setAttribute("xmlns:pi", "ddi:physicalinstance:3_3")
+	
+	var topLevelReference = xmlDoc.createElementNS(nsddi, "ddi:TopLevelReference")
+	topLevelReference.appendChild(createTextNode(xmlDoc, nsr, "r:Agency", "int.example"))
+	topLevelReference.appendChild(createTextNode(xmlDoc, nsr, "r:ID", input.fileName))
+	topLevelReference.appendChild(createTextNode(xmlDoc, nsr, "r:Version", "1.0.0"))
+	topLevelReference.appendChild(createTextNode(xmlDoc, nsr, "r:TypeOfObject", "PhysicalInstance"))
+	fragmentInstance.appendChild(topLevelReference)
 			
-    ddil = ddil + '</codeBook>'
-    return ddil
+	var physicalInstanceFragment = xmlDoc.createElementNS(nsddi, "ddi:Fragment")
+	var physicalInstance = xmlDoc.createElementNS(nsddi, "pi:PhysicalInstance")
+	physicalInstance.appendChild(createTextNode(xmlDoc, nsr, "r:URN", "urn:ddi:int.example:" + input.fileName + ":1.0.0"))
+	physicalInstance.appendChild(createTextNode(xmlDoc, nsr, "r:Agency", "int.example"))
+	physicalInstance.appendChild(createTextNode(xmlDoc, nsr, "r:ID", input.fileName))
+	physicalInstance.appendChild(createTextNode(xmlDoc, nsr, "r:Version", "1.0.0"))
+	physicalInstanceFragment.appendChild(physicalInstance)
+	fragmentInstance.appendChild(physicalInstanceFragment)
+			
+    var prolog = '<?xml version="1.0" encoding="UTF-8"?>'
+    var xmlString = new XMLSerializer().serializeToString(xmlDoc)
+    return prolog + "\n" + formatXml(xmlString)
 }
